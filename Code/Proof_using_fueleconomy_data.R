9c3afd414301ee650236f2eaa8798f05c8c6829f
drbradford12 <denise.bradford@huskers.unl.edu>
2024-08-13 11:49:34 -0400

    Added gower distance and PAM clustering code

--- Code/Proof_using_fueleconomy_data.R
+++ Code/Proof_using_fueleconomy_data.R
@@ -165,3 +165,55 @@ fviz_ellipses(res.famd, c("make", "model"), repel = TRUE)
 
 
 fviz_ellipses(res.famd, 1:2, geom = "point")
+
+
+#### Gower Distance Function ####
+library(cluster)
+
+gower_dist <- daisy(dframe,
+                    metric = "gower",
+                    type = list(logratio = 3))
+summary(gower_dist)
+
+gower_mat <- as.matrix(gower_dist)
+
+# Output most similar pair
+dframe[
+  which(gower_mat == min(gower_mat[gower_mat != min(gower_mat)]),
+        arr.ind = TRUE)[1, ], ]
+
+# Output most dissimilar pair
+dframe[
+  which(gower_mat == max(gower_mat[gower_mat != max(gower_mat)]),
+        arr.ind = TRUE)[1, ], ]
+
+# Calculate silhouette width for many k using PAM
+sil_width <- c(NA)
+for(i in 2:10){
+
+  pam_fit <- pam(gower_dist,
+                 diss = TRUE,
+                 k = i)
+
+  sil_width[i] <- pam_fit$silinfo$avg.width
+
+}
+
+# Plot sihouette width (higher is better)
+plot(1:10, sil_width,
+     xlab = "Number of clusters",
+     ylab = "Silhouette Width")
+lines(1:10, sil_width)
+
+##### PAM Clustering Method ####
+pam_fit <- pam(gower_dist, diss = TRUE, k = 5)
+
+pam_results <- dframe %>%
+  #dplyr::select(-name) %>%
+  mutate(cluster = pam_fit$clustering) %>%
+  group_by(cluster) %>%
+  do(the_summary = summary(.))
+
+pam_results$the_summary
+
+dframe[pam_fit$medoids, ]
