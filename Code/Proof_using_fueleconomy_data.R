b7f515c22bf9d8d514d9778b76369f3bfbf32e49
Denise Bradford <52838109+drbradford12@users.noreply.github.com>
2024-09-02 16:56:23 -0400

    Updating code for Susan

--- Code/Proof_using_fueleconomy_data.R
+++ Code/Proof_using_fueleconomy_data.R
@@ -15,7 +15,8 @@ test_data <- vehicles %>% #From the fueleconomy package
          class = factor(class),
          trans = factor(trans),
          drive = factor(drive),
-         fuel = factor(fuel))
+         fuel = factor(fuel),
+         cyl = factor(cyl))
 
 # I need to split the dataset to get through the examples
 small_test_data <- vehicles %>% #From the fueleconomy package
@@ -31,7 +32,36 @@ small_test_data <- vehicles %>% #From the fueleconomy package
          class = factor(class),
          trans = factor(trans),
          drive = factor(drive),
-         fuel = factor(fuel))
+         fuel = factor(fuel),
+         cyl = factor(cyl))
+
+# Summarize the data by Make removing the model variable using the small data filter
+make_small_test_data <- vehicles %>% #From the fueleconomy package
+  filter(year >= 2010) %>%
+  mutate(trans = ifelse(is.na(trans), "None", trans),
+         cyl = ifelse(is.na(cyl), 0, cyl),
+         displ = ifelse(is.na(displ), 0, displ),
+         make = ifelse(make == "London Taxi", "London Taxi Co", make),
+         fuel_four_cat = case_when(
+             fuel %in% c('Regular Gas and Electricity', 'Premium Gas or Electricity', 'Premium and Electricity') ~ "Hybrid",
+             fuel %in% c('Premium', 'Midgrade', 'Regular')  ~ "Gas Powered",
+             fuel %in% c('Premium or E85', 'Gasoline or E85', 'Diesel', 'CNG')  ~ "Other Gas Powered",
+             fuel %in% c('Electricity')  ~ "Electricity",
+             .default = "other"
+           )
+  ) %>%
+  mutate(make = factor(make),
+         year = factor(year),
+         class = factor(class),
+         trans = factor(trans),
+         drive = factor(drive),
+         fuel_four_cat = factor(fuel_four_cat),
+         cyl = factor(cyl)) %>%
+  group_by(make, year, class, drive, fuel_four_cat, trans, cyl) %>%
+  summarize(mean_displ = round(mean(displ, na.rm = TRUE), 2),
+            mean_hwy = round(mean(hwy, na.rm = TRUE), 2),
+            mean_cty = round(mean(cty,na.rm = TRUE ), 2))
+
 
 
 #### GGPCP Visualizations ####
@@ -39,17 +69,45 @@ library(ggpcp)
 
 p1 <- small_test_data %>%
   pcp_select(2:9, 11:12) %>%
-  pcp_scale(method="uniminmax") %>%
+  pcp_scale() %>%
   pcp_arrange() %>%
   ggplot(aes_pcp()) +
   geom_pcp_axes() +
   #geom_pcp_boxes(boxwidth = 0.1, fill="grey70") +
   geom_pcp(aes(colour = fuel), overplot = "none")
 
+# Adjusted for order in the PCP methods
+small_test_data %>%
+  pcp_select(2:9, 11:12) %>%
+  pcp_scale() %>%
+  pcp_arrange(method="from-both") %>%
+  ggplot(aes_pcp()) +
+  geom_pcp_boxes(boxwidth=0.1) +
+  geom_pcp(aes(colour = fuel), alpha = 0.1)
+
+small_test_data %>%
+  pcp_select(2:9, 11:12) %>%
+  pcp_scale() %>%
+  pcp_arrange(method="from-right") %>%
+  ggplot(aes_pcp()) +
+  geom_pcp_boxes(boxwidth=0.1) +
+  geom_pcp(aes(colour = hwy), alpha = 0.1) +
+  scale_colour_gradient2("hwy", mid="grey80", midpoint = 40, low="darkred", high="darkblue") +
+  theme_bw() +
+  facet_grid(hwy>40~., labeller = "label_both")
+
+p1 <- make_small_test_data %>%
+  pcp_select(1:4, 6:10) %>%
+  pcp_scale() %>%
+  pcp_arrange(method="from-both") %>%
+  ggplot(aes_pcp()) +
+  geom_pcp_boxes(boxwidth=0.1) +
+  geom_pcp(aes(colour = fuel_four_cat), alpha = 0.3, overplot = "none")
+
 
 #### Dimension Reduction Techniques #####
 # Remove duplicate rows
-test_data_unique <- unique( small_test_data[,-1] )
+test_data_unique <- unique( make_small_test_data)
 dframe <- test_data_unique[complete.cases(test_data_unique),]
 
 #### t-SNE Dimension Reduction ####
@@ -96,31 +154,34 @@ for (i in 1:ncol(kprototype_df)){
   colnames(kprototype_df)[i] <- paste0("X", i-1)
 }
 
+
 # model.matrix(~0+., data = kprototype_df) %>%
 #   cor(use = "pairwise.complete.obs") %>%
 #   ggcorrplot(show.diag = FALSE, type = "full", tl.cex = 7, lab = TRUE,
 #              lab_size = 1.5) +
 #   labs(title = "Correlation Heat Map of Used Variables *high-correlation deleted")
 
-fuel_clusters <- kproto(kprototype_df, 5)
+fuel_clusters <- kproto(kprototype_df, 4)
+
 fuel_clusters
 
+
 #summary of each variable
-summary(fuel_clusters)
+#summary(fuel_clusters)
 
 fit_df <- factor(fuel_clusters$cluster, order =  TRUE,
-                 levels = c(1:5))
-fit <- data.frame(dframe, fit_df)
+                 levels = c(1:4))
+fit <- data.frame(dframe, fit_df) %>%
+  mutate(clusters = factor(fit_df))
 
-fit %>%
-  #arrange(fit_df) %>%
-  pcp_select(1:8,10:11) %>%
-  pcp_scale(method="uniminmax") %>%
-  pcp_arrange(fit_df, method = "from-both") %>%
-  ggplot(aes_pcp()) +
-  geom_pcp_axes() +
-  #geom_pcp_boxes(boxwidth = 0.1, fill="grey70") +
-  geom_pcp(aes(colour = fuel), overplot = "none")
+# fit %>%
+#   pcp_select(1:8,10:11,13) %>%
+#   #pcp_scale(method="uniminmax") %>%
+#   pcp_arrange(clusters, method = "from-both") %>%
+#   ggplot(aes_pcp()) +
+#   geom_pcp_axes() +
+#   #geom_pcp_boxes(boxwidth = 0.1, fill="grey70") +
+#   geom_pcp(aes(colour = fuek), overplot = "none")
 
 result_df <- fuel_clusters$centers
 member <- fuel_clusters$size
@@ -131,13 +192,13 @@ require(gridExtra)
 
 p2 <- fit %>%
   #arrange(fit_df) %>%
-  pcp_select(1:11) %>%
-  pcp_scale(method="uniminmax") %>%
-  pcp_arrange() %>%
+  pcp_select(1:4,6:10) %>%
+  pcp_scale() %>%
+  pcp_arrange(method="from-both") %>%
   ggplot(aes_pcp()) +
   geom_pcp_axes() +
   #geom_pcp_boxes(boxwidth = 0.1, fill="grey70") +
-  geom_pcp(aes(colour = fit_df), overplot = "none")
+  geom_pcp(aes(colour = clusters), alpha = 0.3, overplot = "none")
 
 
 grid.arrange(p1, p2, ncol=2)
@@ -277,4 +338,68 @@ obj <- PCAmix(X.quanti=X1,X.quali=X2,graph=FALSE,ndim=5)
 head(obj$quanti.cor)
 
 test<-1:5
+
+
+
+#### Replot using the gower_dist ####
+
+gower_fuel_clusters <- kproto(kprototype_df, type = "gower", 5)
+gower_fuel_clusters
+
+gower_fit_df <- factor(gower_fuel_clusters$cluster, order =  TRUE,
+                 levels = c(1:5))
+
+gower_fit <- data.frame(dframe, gower_fit_df) %>%
+  mutate(clusters = factor(gower_fit_df))
+
+gower_result_df <- gower_fuel_clusters$centers
+gower_member <- gower_fuel_clusters$size
+gower_result <- data.frame(gower_member, gower_result_df)
+gower_result
+
+p3 <- gower_fit %>%
+  #arrange(fit_df) %>%
+  pcp_select(1:4,6:10) %>%
+  pcp_scale() %>%
+  pcp_arrange(method="from-both") %>%
+  ggplot(aes_pcp()) +
+  geom_pcp_axes() +
+  #geom_pcp_boxes(boxwidth = 0.1, fill="grey70") +
+  geom_pcp(aes(colour = clusters), alpha = 0.3, overplot = "none")
+
+grid.arrange(p1, p3, ncol=2)
+
+p4 <- gower_fit %>%
+  pcp_select(12,6:10)  %>%
+  pcp_scale() %>%
+  pcp_arrange(method="from-right") %>%
+  ggplot(aes_pcp()) +
+  geom_pcp_boxes(boxwidth=0.1) +
+  geom_pcp(aes(colour = fuel_four_cat), alpha = 0.1, axiswidth = c(0,0)) +
+  scale_colour_manual(values=c("darkgreen","darkred","darkorange","darkblue", "steelblue")) +
+  guides(colour=guide_legend(override.aes = list(alpha=1))) +
+  geom_pcp_labels() +
+  scale_x_discrete(expand = expansion(add=0.2)) +
+  theme_bw() +
+  facet_grid(str_detect(trans, 'Auto')~., labeller = "label_both")
+
+p <- make_small_test_data %>%
+  pcp_select(5:10)  %>%
+  pcp_scale() %>%
+  pcp_arrange(method="from-right") %>%
+  ggplot(aes_pcp()) +
+  geom_pcp_boxes(boxwidth=0.1) +
+  geom_pcp(aes(colour = fuel_four_cat), alpha = 0.1, axiswidth = c(0,0)) +
+  scale_colour_manual(values=c("darkgreen","darkred","darkorange","darkblue", "steelblue")) +
+  guides(colour=guide_legend(override.aes = list(alpha=1))) +
+  geom_pcp_labels() +
+  scale_x_discrete(expand = expansion(add=0.2)) +
+  theme_bw() +
+  facet_grid(str_detect(trans, 'Auto') ~., labeller = "label_both")
+
+grid.arrange(p, p4, ncol=2)
+
+
+
+
 
